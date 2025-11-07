361cc6e77afc302f02c0e59ed9de2f38c04f6541
Santiago <santiago.gallego28@outlook.com>
2025-11-07 17:11:41 -0500

    feat: estructura básica del proyecto Java

---
+++ src/VoidFunction.java
@@ -0,0 +1,9 @@
+public class VoidFunction {
+    public static void main(String[] args) {
+        saludar();
+    }
+
+    public static void saludar() {
+        System.out.println("¡Hola, mundo!");
+    }
+}
